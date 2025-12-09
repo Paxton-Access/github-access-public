c213e262bee511330631779b9c558a6066a51063
Maciej Maciejewski <maciej.maciejewski@paxton-access.co.uk>
2025-12-09 14:38:45 +0000

    Fix security vuln

--- token.js
+++ token.js
@@ -1,31 +1,39 @@
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
-const axios = require('axios');
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
+const axios = require("axios");
 const args = process.argv.slice(2);
 
-var key = fs.readFileSync('./git.pem');
+var key = fs.readFileSync("./git.pem");
 
 // Create token
-var token = jwt.sign({
+var token = jwt.sign(
+  {
     iat: Math.floor(Date.now() / 1000),
     // JWT expiration time (30 minute maximum)
-    exp: Math.floor(Date.now() / 1000) + (9 * 60),
-    iss: args[0]
-}, key, { algorithm: 'RS256' });
+    exp: Math.floor(Date.now() / 1000) + 30 * 60,
+    iss: args[0],
+  },
+  key,
+  { algorithm: "RS256" }
+);
 
-axios.post(args[1], {},
+axios
+  .post(
+    args[1],
+    {},
     {
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Accept": "application/vnd.github.v3+json"
-        },
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/vnd.github.v3+json",
+      },
     }
-).then(
+  )
+  .then(
     (response) => {
-        console.log(response.data.token)
+      console.log(response.data.token);
     },
     (error) => {
-        console.error(error.response.status)
-        console.error(error.response.data)
+      console.error(error.response.status);
+      console.error(error.response.data);
     }
-)
+  );
